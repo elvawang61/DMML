a7968665c6e3f813448273fdec0f89d6729b04e5
Zayk512 <zhangyukun512@hotmail.com>
2025-03-30 03:09:36 +0100

    Update SVM.R

--- SVM.R
+++ SVM.R
@@ -9,50 +9,41 @@ data=read.csv("/Users/kk/Desktop/Rstudio/DataMiningMachineLearning/dmml/group_3.
 #Develop success variables (1=successful, 0=unsuccessful)
 data$success=ifelse(data$gross>(2*data$budget)&data$imdb_score>7,1,0)
 
-#Standardised processing
-data$gross=scale(data$gross)
-data$budget=scale(data$budget)
-data$imdb_score=scale(data$imdb_score)
-#Setting the normalisation function
-normalize=function(x){
-  return((x-min(x))/(max(x)-min(x)))
-}
-#Normalisation
-data$gross=normalize(data$gross)
-data$budget=normalize(data$budget)
-data$imdb_score=normalize(data$imdb_score)
-#Save normalised data
-write.csv(data,"processed_data.csv",row.names=FALSE)
+#Construct new variable: sum of actor likes
+data$actor_total_likes=rowSums(data[,c("actor_1_facebook_likes",
+                                       "actor_2_facebook_likes",
+                                       "actor_3_facebook_likes")],na.rm=TRUE)
 
-#Read the normalised data
-processed_data=read.csv("processed_data.csv")
+#Selection of variables for modelling
+vars=c("budget","imdb_score","duration","director_facebook_likes",
+       "actor_total_likes","title_year","success")
+data_model=data[,vars]
+
+#Remove missing values
+data_model=na.omit(data_model)
+
+#Standardised numeric variables (excluding success)
+numeric_vars=setdiff(names(data_model),"success")
+data_model[numeric_vars]=scale(data_model[numeric_vars])
 
 #Setting random seeds to ensure reproducible results
 set.seed(123)
 
 #Divide the dataset into a training set and a test set (80% training set, 20% test set)
-train_index=sample(1:nrow(processed_data),0.8*nrow(processed_data))
-train_data=processed_data[train_index,]
-test_data=processed_data[-train_index,]
+train_index=sample(1:nrow(data_model), 0.8 * nrow(data_model))
+train_data=data_model[train_index, ]
+test_data=data_model[-train_index, ]
 
-#Selection of features for SVM training
-train_features=train_data[,c("budget","gross","imdb_score")]
-test_features=test_data[,c("budget","gross","imdb_score")]
-train_features$success=as.factor(train_data$success)
-test_features$success=as.factor(test_data$success)
-
-#Define the `success` tag
-train_labels=train_data$success
-test_labels=test_data$success
-
-#Make sure `success` is of type factor
-train_labels=as.factor(train_labels)
-test_labels=as.factor(test_labels)
+#Building features and labels
+train_features=train_data[,-which(names(train_data)=="success")]
+test_features=test_data[,-which(names(test_data)=="success")]
+train_labels=as.factor(train_data$success)
+test_labels=as.factor(test_data$success)
 
 #Cross-validation to select the best cost & gamma
-tune_linear=tune.svm(success~.,data=train_features,kernel="linear",
+tune_linear=tune.svm(success~.,data=train_data,kernel="linear",
                      cost=c(0.1, 1, 10, 100))
-tune_rbf=tune.svm(success~.,data=train_features,kernel="radial",
+tune_rbf=tune.svm(success~.,data=train_data,kernel="radial",
                   cost=c(0.1,1,10,100),gamma=c(0.01,0.1,1,5,10))
 
 #Getting the optimal parameters
@@ -64,9 +55,9 @@ print(paste("The best RBF SVM cost:",best_cost_rbf,
             "The best gamma:",best_gamma_rbf))
 
 #Training SVM Models
-svm_linear=svm(success~.,data=train_features,kernel="linear",
+svm_linear=svm(success~.,data=train_data,kernel="linear",
                cost=best_cost_linear,scale=FALSE)
-svm_rbf=svm(success~.,data=train_features,kernel="radial",
+svm_rbf=svm(success~.,data=train_data,kernel="radial",
             cost=best_cost_rbf,gamma=best_gamma_rbf,scale=FALSE)
 
 #Linear SVM Prediction
